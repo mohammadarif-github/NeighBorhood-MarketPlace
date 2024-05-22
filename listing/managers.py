ff771011227ff9f9318fbc313e72c895443e764a
Mohammad Ariful Islam <mohammadarif.it159@gmail.com>
2024-05-22 21:51:12 +0600

    db updated

--- listing/managers.py
+++ listing/managers.py
@@ -16,13 +16,14 @@ class CustomUserManager(BaseUserManager):
         if not password:
             raise ValueError("The Password must be set")
         
-        # email = self.normalize_email(email)
+        # Normalize the email address
+        email = self.normalize_email(email)
         user = self.model(username=username, email=email, first_name=first_name, last_name=last_name, phone=phone, address=address, image=image, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -35,4 +36,8 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
 
-        return self.create_user(username=email, email=email, password=password, **extra_fields)
+        # Provide default values for first_name and last_name if not set
+        extra_fields.setdefault('first_name', 'Admin')
+        extra_fields.setdefault('last_name', 'User')
+
+        return self.create_user(username=username, email=email, password=password, **extra_fields)
