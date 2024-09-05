0cb9492210c9e6df14ea0287c147216735094d34
Mohammad Ariful Islam <mohammadarif.it159@gmail.com>
2024-09-05 11:07:20 +0600

    CORS Updated

--- NeighborhoodShop/settings.py
+++ NeighborhoodShop/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500","https://mohammadarif-github.github.io",
+    "http://127.0.0.1:5500","https://mohammadarif-github.github.io","https://neighborhood-marketplace-869o.onrender.com",
 ]
 
 ROOT_URLCONF = 'NeighborhoodShop.urls'
